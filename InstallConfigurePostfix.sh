c80c467491a1930d40483c424de4795d5b42d38b
Abdelrhman Hamdy <69608603+abdelrhman-hamdy@users.noreply.github.com>
2023-02-19 15:36:57 +0200

    Update InstallConfigurePostfix.sh

--- InstallConfigurePostfix.sh
+++ InstallConfigurePostfix.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+# Example of using the script : ./InstallConfigurePostfix.sh    Hosname       domainname    "Network Subnet"
+# Example of using the script : ./InstallConfigurePostfix.sh  mail.hamdy.com   hamdy.com      "192.168.1.0\/24"
 
-# you can use the script like this :  ./test.sh  mail.jana.com jana.com "192.168.1.0\/24"
 
-
-function EditCongFile(){
+function EditCongFile(){                      # this function takes 3 argument,1- the field needed to change its value, 2- the value , 3 - conf file path 
 sed -i -e  "s/#$1 = .*/$1 = $2/" $3
 }
 
-function CommentLine(){
+# this function is used to comment a line, it has two arguement, the string pattern , and the conf file path  respectively
+function CommentLine(){                        
 sed  -i -e "/^$1/ c#$1" $2
 
 }
-
+# this function is used to Uncomment a line, it has two arguement, the string pattern , and the conf file path respectively
 function UnCommentLine(){
 sed -i -e  "/^#$1/ c$1" $2
 
 }
 conf_file=/etc/postfix/main.cf
 
-if [ "$(rpmquery postfix)" = "package postfix is not installed" ];then yum install postfix -y ;else echo " Postfix is installed"; fi
+if [ "$(rpmquery postfix)" = "package postfix is not installed" ];then yum install postfix -y ;else echo " Postfix is installed"; fi  # install the package if not exists
 
-EditCongFile myhostname $1 $conf_file
+EditCongFile myhostname $1 $conf_file                     #configuring mail server hostname
 
-EditCongFile mydomain   $2  $conf_file
+EditCongFile mydomain   $2  $conf_file                    #configuring mail server domain name
 
-EditCongFile myorigin "\$mydomain" $conf_file
+EditCongFile myorigin "\$mydomain" $conf_file             #configuring mail server origin
 
 UnCommentLine  'inet_interfaces = all' $conf_file
 
@@ -39,7 +40,7 @@ UnCommentLine  'mydestination = $myhostname, localhost.$mydomain, localhost, $my
 
 
 
-sed -i -e  "s/#mynetworks = 1.*/mynetworks = $3, 127.0.0.0\/8/" $conf_file
+sed -i -e  "s/#mynetworks = 1.*/mynetworks = $3, 127.0.0.0\/8/" $conf_file   # configuring mail server accepts requests from the local network
 
 firewall-cmd --add-port=25/tcp --permanent && firewall-cmd --reload
 
